5218c3fd31b614e4cc243ac0c87a0fec4d84b0eb
Laura Haro <lauraharomolina@gmail.com>
2022-12-07 12:33:08 +0100

    script data base

--- script-bbdd/script.sql
+++ script-bbdd/script.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `instituto` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `instituto`;
 -- MySQL dump 10.13  Distrib 8.0.29, for Linux (x86_64)
 --
 -- Host: 127.0.0.1    Database: instituto
@@ -34,7 +32,7 @@ CREATE TABLE `alumnos` (
   `nuevo` tinyint DEFAULT NULL,
   `foto` longblob,
   PRIMARY KEY (`codigo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -46,4 +44,4 @@ CREATE TABLE `alumnos` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-12-07 11:28:37
+-- Dump completed on 2022-12-07 12:32:43
